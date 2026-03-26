a705ed6b0a34598609c73d7fd6f03de0d3de5883
Phylicia Bediako <phylicia.bediako@gmail.com>
2026-03-26 15:35:45 -0600

    Muon and VEML modifications

--- modifiedControlledEnviroAg/src/modifiedControlledEnviroAg.cpp
+++ modifiedControlledEnviroAg/src/modifiedControlledEnviroAg.cpp
@@ -33,10 +33,19 @@
 #include "Adafruit_BME280.h"
 #include "Adafruit_VEML7700.h"
 
-
-
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(SEMI_AUTOMATIC);
+
+// MQTT Broker and Feeds
+TCPClient TheClient; 
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+
+// Device Feeds
+Adafruit_MQTT_Publish tempFeed = Adafruit_MQTT_Publish(&mqtt, "cea/temperature");
+Adafruit_MQTT_Publish humidityFeed = Adafruit_MQTT_Publish(&mqtt, "cea/humidity");
+Adafruit_MQTT_Publish pressureFeed = Adafruit_MQTT_Publish(&mqtt, "cea/pressure");
+Adafruit_MQTT_Publish luxFeed = Adafruit_MQTT_Publish(&mqtt, "cea/lux");
+Adafruit_MQTT_Publish device_01_Feed = Adafruit_MQTT_Publish(&mqtt, "cea/devicenumber01");
 
 struct enviroSensors{
   float tempF;
@@ -44,6 +53,8 @@ struct enviroSensors{
   float baroPressure;
   float luxVal;
 };
+
+//Devices device_1;
 
 enviroSensors enviroData;
 enviroSensors lightData;
@@ -55,6 +66,13 @@ void getBME(enviroSensors bmeData);
 void getLux(enviroSensors luxData);
 
 unsigned int lastRead;
+
+void MQTT_connect();
+void pingBroker();
+uint64_t millis64bit();
+void watchdogHandler();
+void parseIncomingData(const uint8_t *data, Devices deviceNum, int deviceID);
+
 
 /******************************************************************/
 void setup() {
@@ -69,7 +87,7 @@ void setup() {
     Serial.printf("Successfully started BME280\n");
   }
 
-//Lux Sensor: the Lux has a default address so don't need to add an address here
+//Lux Setup (note: the Lux has a default address so don't need to add an address here)
   if(!luxSensor.begin()){ 
     Serial.printf("VEML7700 not recognized\n");
   }
@@ -85,21 +103,15 @@ void setup() {
 
 /******************************************************************/
 void loop () {
- if((millis() - lastRead) > 6000){
-  getBME(enviroData);
-  getLux(lightData);
-  lastRead = millis();
+  MQTT_connect();
+  if((millis() - lastRead) > 6000){
+    getBME(enviroData);
+    getLux(lightData);
+    lastRead = millis();
   //publish data
- }
+  }
   
 }
-
-
-/*****************************************************************
-void sendThenDisconnect() {
-  
-}
-*/
 
 /*****************************************************************/
 void getBME(enviroSensors bmeData) {
@@ -117,4 +129,98 @@ void getLux(enviroSensors luxData) {
   Serial.printf("Lux: %0.2f\n", luxData.luxVal);
   delay(5000);
   //when dark, should be close to 0. when super bright (like with a flashlight) should be ~65K
+}
+
+/*****************************************************************/
+void watchdogHandler(){
+  System.reset(RESET_NO_WAIT);
+}
+
+/*****************************************************************/
+void MQTT_connect() {
+  int8_t ret;
+  if (mqtt.connected()) {
+    
+    return;
+  }
+  Serial.print("Connecting to MQTT... ");
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.printf("%s\n",(char *)mqtt.connectErrorString(ret));
+    Serial.printf("Retrying MQTT connection in 5 seconds..\n");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+  }
+  Serial.printf("MQTT Connected!\n");
+  }
+
+/*****************************************************************/
+void pingBroker(){
+  static uint64_t last = millis64bit();
+  if ((millis()-last)>120000) {
+    Serial.printf("Pinging MQTT \n");
+    if(! mqtt.ping()) {
+      Serial.printf("Disconnecting \n");
+      mqtt.disconnect();
+    }
+      last = millis();
+  }
+}
+
+/*****************************************************************/
+uint64_t millis64bit() {
+    static uint32_t low4bytes, high4bytes;
+    uint32_t newMillis;
+
+    newMillis = millis();
+    if (newMillis < low4bytes) {    //check if millis has rolled over
+       high4bytes++;                //if so, add one to high bytes
+    }
+    low4bytes = newMillis;
+    return (high4bytes << 32 | low4bytes); //return 64-bit (8-byte) millis
+}
+
+/*****************************************************************/
+void parseIncomingData(const uint8_t *data, Devices deviceNum, int deviceID, Adafruit_MQTT_Publish feedName[14][12]){
+
+  String incomingMessage;
+  int newDelimiter;
+  incomingMessage = (String)(char *)data;
+  // Serial.printf("%s\n", incomingMessage.c_str());
+
+  // Parse Temperature
+  newDelimiter = incomingMessage.indexOf(':');
+  //Serial.printf("%i\n", newDelimiter);
+  String temperature = incomingMessage.substring(0,newDelimiter);
+  incomingMessage.remove(0,newDelimiter+1);
+  deviceNum.tempF = atof(temperature.c_str());
+
+  // Parse humidity
+  newDelimiter = incomingMessage.indexOf(':');
+  //Serial.printf("%i\n", newDelimiter);
+  String humidity = incomingMessage.substring(0,newDelimiter);
+  incomingMessage.remove(0,newDelimiter+1);
+  deviceNum.humidity = atoi(humidity.c_str());
+
+  // Parse barometric pressure
+  newDelimiter = incomingMessage.indexOf(':');
+  //Serial.printf("%i\n", newDelimiter);
+  String pressure = incomingMessage.substring(0,newDelimiter);
+  incomingMessage.remove(0,newDelimiter+1);
+  deviceNum.pressure = atof(pressure.c_str());
+
+  // Parse Lux reading
+  newDelimiter = incomingMessage.indexOf(':');
+  //Serial.printf("%i\n", newDelimiter);
+  String luxVal = incomingMessage.substring(0, newDelimiter);
+  incomingMessage.remove(0,newDelimiter+1);
+  deviceNum.luxReading = atof(luxVal.c_str()); 
+
+  Serial.printf("Readings from device ID: %i\nTemperature: %0.1f\nHumidity %i\nBarometric pressure: %0.1f\nLux: %0.4f\n", deviceID, deviceNum.tempF ,deviceNum.humidity, deviceNum.pressure, deviceNum.luxReading;
+  if(mqtt.Update()){
+    feedName[deviceID-1][0].publish(deviceNum.tempF);
+    feedName[deviceID-1][1].publish(deviceNum.humidity);
+    feedName[deviceID-1][2].publish(deviceNum.pressure);
+    feedName[deviceID-1][3].publish(deviceNum.luxReading);
+    feedName[deviceID-1][11].publish(deviceID);
+  }
 }
